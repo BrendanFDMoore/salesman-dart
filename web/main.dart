5437d5481052677bf1f32035a9f1aee93254f8ea
Brendan <git@bfdmoore.com>
2016-02-22 01:45:16 -0500

    Adding polymer, simple element inclusion to show it works (NOTE: MUST HOST VIA PUB SERVE)

--- web/main.dart
+++ web/main.dart
@@ -2,16 +2,31 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'dart:html';
+import 'package:paper_elements/paper_button.dart';
+import 'package:polymer/polymer.dart';
 
 CanvasElement canvas;
 CanvasRenderingContext2D context; 
 
-void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
-  canvas = querySelector('#mainCanvas');
-  context = canvas.getContext('2d');
-  context.moveTo(100, 150);
-  context.lineTo(450, 50);
-  context.lineWidth = 4;
-  context.stroke();
+void main() async{
+	await initPolymer();
+
+
+	querySelector('#output').text = 'Your Dart app is running.';
+  	canvas = querySelector('#mainCanvas');
+  	context = canvas.getContext('2d');
+
+  	querySelector('paper-button').on['click'].listen((_) {
+	    print('Button clicked!');
+	    //console.log('Button click logged!');
+	    context.moveTo(200, 150);
+	  	context.lineTo(550, 50);
+	  	context.lineWidth = 4;
+	  	context.stroke();
+	});
+
+  	context.moveTo(100, 150);
+  	context.lineTo(450, 50);
+  	context.lineWidth = 4;
+  	context.stroke();
 }
